808765dd555141f66cd80628836e6b772922e643
trevorfaivre <trevorfaivre@hotmail.com>
2023-01-10 15:21:51 -0600

    created Header and Home components

---
+++ src/firebas.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD9dwaUUmz7Fo8SgVXva7rxkxhpfXe2-Zs",
+    authDomain: "challenge-9e41c.firebaseapp.com",
+    projectId: "challenge-9e41c",
+    storageBucket: "challenge-9e41c.appspot.com",
+    messagingSenderId: "38660478199",
+    appId: "1:38660478199:web:e80750d1fd862c825eaff2",
+    measurementId: "G-N03YLZJ068"
+  };
